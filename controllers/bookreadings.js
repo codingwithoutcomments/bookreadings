f05eb0f387b19bff7a540612158d5049cdebb872
chreath <chris.x.reath.-nd@disney.com>
2014-06-30 16:05:03 -0700

    new baseline

---
+++ controllers/bookreadings.js
@@ -0,0 +1,5 @@
+angular.module("bookreadings")
+	.constant("firebaseURL", "https://bookreadings.firebaseio.com")
+	.controller("bookreadingsCtrl", function ($scope, $firebase, $http, $location, firebaseURL) {
+		var i = 0;
+	});
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var connect = require('connect');
 
 connect.createServer(
-	connect.static("../new_example")
+	connect.static("../bookreadings")
 ).listen(5000);
