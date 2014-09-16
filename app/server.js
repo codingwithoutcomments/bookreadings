0e8e999d140f3ba307f771a258522f5763956160
chreath <chris.x.reath.-nd@disney.com>
2014-09-16 10:23:13 -0700

    Update with node static

---
+++ app/server.js
@@ -0,0 +1,12 @@
+var static = require('node-static');
+
+var file = new static.Server('../app');
+
+require('http').createServer(function (request, response) {
+    request.addListener('end', function () {
+        //
+        // Serve files!
+        //
+        file.serve(request, response);
+    }).resume();
+}).listen(5000);
